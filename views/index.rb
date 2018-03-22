721c14527cbe7faab88c97804f50e5b6457eceda
benjaminmortimer <30536192+benjaminmortimer@users.noreply.github.com>
2018-03-22 22:50:43 +0000

    Create index.rb

---
+++ views/index.rb
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Toothtime</title>
+	</head>
+	<body>
+		<h1>Toothtime</h1>
+
+		<% if done %>
+
+		<p>Done!</p>
+		
+		<% else %>
+		
+		<form action="/done" method="post">
+			<label>
+			In or out?
+				<input type="text" name="in_out">
+			</label>
+			<label>
+			Date:
+				<input type="text" name="date">
+			</label>
+			<label>
+			Time:
+				<input type="text" name="time">
+			</label>
+		<input type="submit">
+		</form>
+
+		<% end %>
+
+		<a href="/show">Show all times</a>
+
+	</body>
+</html>
